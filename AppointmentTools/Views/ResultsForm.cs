f8275d4ea98727f8e71da4365f9b49f2a3f2517e
Zach <91436608+ZZumMallen@users.noreply.github.com>
2026-07-26 02:02:42 -0500

    Basic Results form is completed

---
+++ AppointmentTools/Views/ResultsForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppointmentTools.Views {
+    public partial class ResultsForm : Form {
+
+        public ResultsForm(string from, string to, string driveTime, string policyLine, bool meetsPolicyStandard) {
+            InitializeComponent();            
+            Label_HeaderFrom.Text = $"From: {from}";
+            Label_HeaderTo.Text = $"To: {to}";
+            Label_HeaderTime.Text = $"Time: {driveTime}";            
+            Label_PolicyLine.Text = policyLine;            
+
+            if(meetsPolicyStandard) {
+                Label_PolicyLine.BackColor = Color.Green;
+            }
+            else {
+                Label_PolicyLine.BackColor = Color.Red;
+            }
+        }
+
+
+
+        private void Button_Ok_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+    }
+}
